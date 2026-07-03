35151ad14f478fb29c90e03b5bf391bd0a77c31d
baeikdo <baeikdo@gmail.com>
2026-07-03 22:59:31 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,1 @@
-const CACHE='jujeon-fishing-pwa-v1';
-const ASSETS=['./','./index.html','./app.js','./manifest.json','./icon-192.png','./icon-512.png'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const CACHE='dokjon-v02';const A=['./','./index.html','./app.js','./manifest.json','./icon-192.png','./icon-512.png'];self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(A))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
